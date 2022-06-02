c3fe2c6bdb3527e136c07844389e96f0db8bf44c
enyichiaagu <enyichiaagu@gmail.com>
2022-06-03 00:26:54 +0100

    Solved bug in styling

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -5,46 +5,36 @@ const Homepage = () => (
 		<div className="directory-menu">
 			<div className="menu-item">
 				<div className="content">
-					<div className="title">
-						<h1>HATS</h1>
-						<span>SHOP NOW</span>
-					</div>
+						<h1 className="title">HATS</h1>
+						<span className="subtitle">SHOP NOW</span>
 				</div>
 			</div>
 			<div className="menu-item">
 				<div className="content">
-					<div className="title">
-						<h1>JACKETS</h1>
-						<span>SHOP NOW</span>
-					</div>
+						<h1 className="title">JACKETS</h1>
+						<span className="subtitle">SHOP NOW</span>
 				</div>
 			</div>
 			<div className="menu-item">
 				<div className="content">
-					<div className="title">
-						<h1>SNEAKERS</h1>
-						<span>SHOP NOW</span>
-					</div>
+						<h1 className="title">SNEAKERS</h1>
+						<span className="subtitle">SHOP NOW</span>
 				</div>
 			</div>
 			<div className="menu-item">
 				<div className="content">
-					<div className="title">
-						<h1>MENS</h1>
-						<span>SHOP NOW</span>
-					</div>
+						<h1 className="title">MENS</h1>
+						<span className="subtitle">SHOP NOW</span>
 				</div>
 			</div>
 			<div className="menu-item">
 				<div className="content">
-					<div className="title">
-						<h1>WOMENS</h1>
-						<span>SHOP NOW</span>
-					</div>
+						<h1 className="title">WOMENS</h1>
+						<span className="subtitle">SHOP NOW</span>
 				</div>
-			</div>
+			</div>  
 		</div>
 	</div>
 )
 
-export default Homepage;
+export default Homepage
