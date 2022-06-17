97334ad518e2ad0068dc29266fab486b6b55cd44
enyichiaagu <enyichiaagu@gmail.com>
2022-06-17 22:53:11 +0100

    Added header component and styled the header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path='/' element={<Homepage />}/>
         <Route path="shop" element={<ShopPage/>}/>
