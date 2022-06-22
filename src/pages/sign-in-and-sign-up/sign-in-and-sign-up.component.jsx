659916a7a027434070d6885c38170e09eb6e110f
enyichiaagu <enyichiaagu@gmail.com>
2022-06-22 08:01:49 +0100

    Added more components to the sign in page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import SignIn from '../../components/sign-in/sign-in.component'
+
 import './sign-in-and-sign-up.styles.scss'
 
 function SignInAndSignUpPage() {
   return (
-    <div className='sign-in-and-sign-up'>SIGN IN</div>
+    <div className='sign-in-and-sign-up'>
+      <SignIn/>
+    </div>
   )
 }
 
