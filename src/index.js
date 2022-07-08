31a30d94421a734e4cd76618d29aef0f10d0adc0
enyichiaagu <enyichiaagu@gmail.com>
2022-07-08 19:06:59 +0100

    Installed redux and set up store, user action and user, user reducer and root reducer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
